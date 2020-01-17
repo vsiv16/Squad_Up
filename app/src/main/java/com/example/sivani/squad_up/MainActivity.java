92d9a8c516cc7fb1e943561dcf582268b9b363da
Sivani <vsivani01@gmail.com>
2020-01-16 18:30:23 -0800

    w/ Preliminary Speech Code

--- app/src/main/java/com/example/sivani/squad_up/MainActivity.java
+++ app/src/main/java/com/example/sivani/squad_up/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.sivani.squad_up;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.speech.RecognizerIntent;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
     private Button mNextButton;
     private Button mLowButton;
     private Button mHighButton;
+    private MediaRecorder fMediaRecorder;
     static final int LOW = 1;
     static final int HIGH = 2;
+    private final int REQ_CODE = 100;
 
 
     @Override
@@ -30,13 +38,13 @@ public class MainActivity extends AppCompatActivity {
         mSpeakButton = (Button) findViewById(R.id.speak_button);
         mSpeakButton.setText(R.string.speak_button);
 
-        mLowButton = (Button)findViewById(R.id.low_button);
+        mLowButton = (Button) findViewById(R.id.low_button);
         mLowButton.setText(R.string.low_button);
 
-        mHighButton = (Button)findViewById(R.id.high_button);
+        mHighButton = (Button) findViewById(R.id.high_button);
         mHighButton.setText(R.string.high_button);
 
-        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
         mNextButton.setText(R.string.next_button);
         mNextButton.setEnabled(false);
 
@@ -44,6 +52,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // add recording and speech to text code here
+
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Need to speak");
+                try {
+                    startActivityForResult(intent, REQ_CODE);
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getApplicationContext(),
+                            "Sorry your device not supported",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -79,5 +100,19 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQ_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    mWelcomeTextView.setText("hi");       // result.get(0));
+                }
+                break;
+            }
+        }
 
+
+    }
 }
